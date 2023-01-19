f2a267e95f8be57df7e60db69b86edb7f5621a5e
Javi <javiermespino@gmail.com>
2023-01-19 23:00:05 +0000

    Insert VALUES

--- database/bbdd.sql
+++ database/bbdd.sql
@@ -2,23 +2,17 @@
 CREATE DATABASE IF NOT EXISTS tienda;
 USE tienda;
 
--- Creamos un usuario y le damos todos los permisos
-CREATE USER IF NOT EXISTS 'javi'@'localhost' IDENTIFIED BY '12345';
-
-GRANT ALL PRIVILEGES ON tienda.* TO javi@'localhost' IDENTIFIED BY '12345';
-FLUSH PRIVILEGES;
-
 -- Empezamos a crear las tablas
-CREATE TABLE IF NOT EXISTS usuario (
+CREATE TABLE IF NOT EXISTS usuarios (
     id_user INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nick VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL,
     email VARCHAR(50) NOT NULL,
     phone INT(9) NOT NULL,
-    admin BOOLEAN NOT NULL
+    admin BOOLEAN NOT NULL DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS producto (
+CREATE TABLE IF NOT EXISTS productos (
     id_product INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     price DEC(5, 2) NOT NULL,   -- Ejemplo: 999,99
@@ -30,7 +24,36 @@ CREATE TABLE IF NOT EXISTS carrito (
     id_carrito INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_user INT(11) NOT NULL,
     id_product INT(11) NOT NULL,
-    purchase_date DATE NOT NULL
+    purchase_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- DROP DATABASE IF EXISTS tienda;
+
+-- Insertamos un admin
+INSERT INTO usuarios (nick, password, email, phone, admin) VALUES (
+    "admin", "admin", "admin@gmail.com", 111111111, 1
+);
+
+-- Insertamos usuarios
+INSERT INTO usuarios (nick, password, email, phone) VALUES 
+    ("Javi", "123", "javi@gmail.com", 123456789),
+    ("Test", "123", "test@gmail.com", 987654321);
+
+-- Insertamos productos
+INSERT INTO productos (name, price, description, image) VALUES
+    ("Black Forest Pastry", 12, "Cake - Pastries", "../images/img-template/cake-item1.jpg"),
+    ("Sprinkled Donuts", 12, "Cake - Pastries", "../images/img-template/cake-item2.jpg"),
+    ("Black Forest Cupcake", 12, "Cake - Pastries", "../images/img-template/cake-item3.jpg"),
+    ("Macarons with Berries", 12, "Cake - Pastries", "../images/img-template/cake-item4.jpg");
+
+-- Insertamos una compra
+INSERT INTO carrito (id_user, id_product) VALUES
+    (2, 2),
+    (3, 3);
+
+-- Comprobamos el carrito
+SELECT usuarios.nick, productos.name 
+FROM usuarios INNER JOIN carrito
+ON usuarios.id_user = carrito.id_user
+INNER JOIN productos
+ON productos.id_product = carrito.id_product;
