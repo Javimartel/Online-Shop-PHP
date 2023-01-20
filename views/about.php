e73d71c421a06d6b51abfc4daab464b9b11af5a3
Javi <javiermespino@gmail.com>
2023-01-20 00:37:47 +0000

    Modified header and footer includes

--- views/about.php
+++ views/about.php
@@ -1,53 +1,4 @@
-<!DOCTYPE html>
-	<html>
-	<head>
-		<title>Javi - Tienda</title>
-		<meta charset="utf-8">
-	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	    <meta name="format-detection" content="telephone=no">
-	    <meta name="apple-mobile-web-app-capable" content="yes">
-	    <meta name="author" content="">
-	    <meta name="keywords" content="">
-	    <meta name="description" content="">
-		
-	    <link rel="stylesheet" type="text/css" href="../fonts/icomoon/icomoon.css">
-	    <link rel="stylesheet" type="text/css" href="../css/slick.css"/>
-		<link rel="stylesheet" type="text/css" href="../css/slick-theme.css"/>
-		<link rel="stylesheet" type="text/css" href="../css/magnific-popup.css"/>
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-		<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css"/>
-	    <link rel="stylesheet" type="text/css" href="../css/style.css">
-
-	</head>
-	<body>
-
-<div id="main-wrapper" class="overflow-hidden">
-	<div class="header-top-menu bg-black color-secondary">
-		<div class="container">
-			<div class="row">
-				<div class="wrap flex-container padding-small">
-				<div class="contact-block">
-					<span class="pr-50"><i class="icon icon-phone"></i>123456789</span>
-					<span class="color-secondary"><i class="icon icon-envelope-o"></i><a href="#"> javi@gmail.com</a></span>
-				</div>
-
-				<div class="header-top-right">
-					<ul class="list-unstyled d-flex color-secondary">
-						<li class="pr-50"><a href="#">Mi Cuenta</a></li>
-						<li class="pr-50"><a href="#">FAQs</a></li>
-						<li><a href="#">Ayuda</a></li>
-					</ul>
-				</div>
-
-				</div><!--flex-container-->
-
-			</div>
-		</div>
-	</div><!----top-menu---->
+<?php include "./parts/header-views.php";?>
 
 	<header id="header">
 		<div class="container">
@@ -404,11 +355,4 @@
 
 </div>    	
 
-<script src="../js/jquery-1.11.0.min.js"></script>
-	<script type="text/javascript" src="../js/bootstrap.bundle.min.js"></script>
-	<script type="text/javascript" src="../js/slick.min.js"></script>
-	<script type="text/javascript" src="../js/jquery.magnific-popup.min.js"></script>
-	<script type="text/javascript" src="../js/script.js"></script>
-
-</body>
-</html>	
+<?php include "./parts/footer-views.php";?>
