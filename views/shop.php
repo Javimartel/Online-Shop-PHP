77f56af42f1e344ea60a84c8c854501437ad9179
Javi <javiermespino@gmail.com>
2023-01-21 13:54:46 +0000

    Added admin insert

--- views/shop.php
+++ views/shop.php
@@ -104,6 +104,16 @@
 								</div>
 							</div>";
 					}
+					if (isset($_SESSION["user"]) && $_SESSION["user"]["admin"] === 1) {
+						echo '<div class="product-item col-md-3 pb-4">
+							<a href="#">
+							<div class="d-flex flex-column justify-content-center align-items-center" style="width:265px;height:303.8px;">
+							<svg class="d-flex align-center" xmlns="http://www.w3.org/2000/svg" width="150" height="150" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+								<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+								<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+							  </svg>
+							</div></a></div>';
+					}
 		
 		
 				} catch (PDOException $error) {}
