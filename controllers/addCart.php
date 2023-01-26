0d98f3b54640f52ab98dcb51b24d0eafa2a67469
Javi <javiermespino@gmail.com>
2023-01-26 15:15:27 +0000

    Controller add cart

---
+++ controllers/addCart.php
@@ -0,0 +1,20 @@
+<?php
+    $config = include "../config/config.php";
+    try {
+        $dsn = 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'];
+        $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+
+        // Coger el ID del usuario que está conectado
+        $consultaSQL = 'SELECT id_user FROM usuarios WHERE nick = "'.$_SESSION["user"]["nick"].'"';
+        $sentencia = $conexion->prepare($consultaSQL);
+        $sentencia->execute();
+        $id_user = $sentencia->fetchAll();
+
+        // Añadir producto al carrito con el id del usuario
+        $consultaSQL = "INSERT INTO carrito (id_user, id_product)";
+        $consultaSQL .= "VALUES (" . $id_user[0][0] .", ". $_POST["id-product"] . ")";
+        $sentencia = $conexion->prepare($consultaSQL);
+        $sentencia->execute();
+
+    } catch (PDOException $error) {}
+?>
--- views/shop.php
+++ views/shop.php
@@ -26,24 +26,8 @@
 	}
 
 	if (isset($_POST["add-product"]) && isset($_SESSION["user"])) {
-		$config = include "../config/config.php";
-		try {
-			$dsn = 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'];
-			$conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-
-			// Coger el ID del usuario que está conectado
-			$consultaSQL = 'SELECT id_user FROM usuarios WHERE nick = "'.$_SESSION["user"]["nick"].'"';
-			$sentencia = $conexion->prepare($consultaSQL);
-			$sentencia->execute();
-			$id_user = $sentencia->fetchAll();
-
-			// Añadir producto al carrito con el id del usuario
-			$consultaSQL = "INSERT INTO carrito (id_user, id_product)";
-			$consultaSQL .= "VALUES (" . $id_user[0][0] .", ". $_POST["id-product"] . ")";
-			$sentencia = $conexion->prepare($consultaSQL);
-            $sentencia->execute();
-
-		} catch (PDOException $error) {}
+		// Añadimos producto al carrito
+		include "../controllers/addCart.php";
 	}
 
 	if(isset($_SESSION["user"])) {
