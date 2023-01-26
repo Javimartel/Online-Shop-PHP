441756a42cf6352e246b4227829b0280d00519f3
Javi <javiermespino@gmail.com>
2023-01-26 15:13:54 +0000

    Controller remove cart

---
+++ controllers/removeCart.php
@@ -0,0 +1,13 @@
+<?php
+    $config = include "../config/config.php";
+    try {
+        $dsn = 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'];
+        $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
+
+        // Eliminamos del carrito
+        $consultaSQL = 'DELETE FROM carrito WHERE id_product = "'.$_POST["id-product"].'" AND purchase_date = "'.$_POST["purchase-date"].'"';
+        $sentencia = $conexion->prepare($consultaSQL);
+        $sentencia->execute();
+
+    } catch (PDOException $error) {}
+?>
--- views/shop.php
+++ views/shop.php
@@ -21,17 +21,8 @@
 	}
 
 	if (isset($_POST["remove-carrito"])) {
-		$config = include "../config/config.php";
-		try {
-			$dsn = 'mysql:host='.$config['db']['host'].';dbname='.$config['db']['name'];
-			$conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
-
-			// Eliminamos del carrito
-			$consultaSQL = 'DELETE FROM carrito WHERE id_product = "'.$_POST["id-product"].'" AND purchase_date = "'.$_POST["purchase-date"].'"';
-			$sentencia = $conexion->prepare($consultaSQL);
-            $sentencia->execute();
-
-		} catch (PDOException $error) {}
+		// Eliminamos producto del carrito
+		include "../controllers/removeCart.php";
 	}
 
 	if (isset($_POST["add-product"]) && isset($_SESSION["user"])) {
