ef33298fd6722c55b41fea19a758f587417540bf
Javi <javiermespino@gmail.com>
2023-01-30 16:35:18 +0000

    Added dotenv

--- config/config.php
+++ config/config.php
@@ -1,10 +1,17 @@
 <?php
+    // Si no existe la variable de Heroku, se carga de manera local
+    if (!isset($_ENV['CLEARDB_DATABASE_URL'])) {
+        require __DIR__ . '/../vendor/autoload.php';
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+        $dotenv->load();
+    }
+
     return [
         'db' => [
-            'host' => 'localhost',
-            'user' => 'root',
-            'pass' => '',
-            'name' => 'tienda',
+            'host' => $_ENV['DB_HOST'],
+            'user' => $_ENV['DB_USER'],
+            'pass' => $_ENV['DB_PASS'],
+            'name' => $_ENV['DB_NAME'],
             'options' => [
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
             ]
